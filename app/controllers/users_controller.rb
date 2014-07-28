6b25386b11a806554f42366530c7f5c1ba934c0a
Bruno Faúndez <gameover_sxt_nah@hotmail.com>
2014-07-28 14:43:35 -0400

    La navbar ahora es responsive y sensual

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,14 @@ class UsersController < ApplicationController
   end
 
   def new
+    redirect_to new_user_registration_path
   end
 
   def create
   end
 
   def edit
+    redirect_to edit_user_registration_path(params[:id])
   end
 
   def update
